ed2861be69c8e693781a8c20264ef40853e7522b
Gitlab auto-deploy <leeppolis@gmail.com>
2019-03-15 14:51:35 +0000

    Ready to deploy

---
+++ precache-manifest.e28c1906a2a26afde52ad27acc7cceca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87e84274601fbf5954a7",
+    "url": "/india2019/static/css/main.57752ce6.chunk.css"
+  },
+  {
+    "revision": "87e84274601fbf5954a7",
+    "url": "/india2019/static/js/main.87e84274.chunk.js"
+  },
+  {
+    "revision": "5a2ad94f8fa693b6508d",
+    "url": "/india2019/static/js/runtime~main.5a2ad94f.js"
+  },
+  {
+    "revision": "13544b68cbe7aee39203",
+    "url": "/india2019/static/js/2.13544b68.chunk.js"
+  },
+  {
+    "revision": "88d818cc3719ec6cff546d11c56820f4",
+    "url": "/india2019/index.html"
+  }
+];
